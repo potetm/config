0e48d8e91ab8e604e0e6b86a604391cfe7d1ac19
Timothy Pote <pote.timothy@gmail.com>
2017-08-14 14:16:23 -0500

    Sutff

--- brew/brew-base/etc/profile.d/java.sh
+++ brew/brew-base/etc/profile.d/java.sh
@@ -1,3 +1,3 @@
 export JAVA_HOME="/opt/java/home"
-export JVM_OPTS="-Xmx6g"
+export JVM_OPTS="-Xmx6g -XX:MaxPermSize=256M"
 export PATH="$JAVA_HOME/bin:$PATH"
--- brew/brew-base/etc/profile.d/pyenv-virtualenv.sh
+++ brew/brew-base/etc/profile.d/pyenv-virtualenv.sh
@@ -1,3 +1,5 @@
-if which pyenv-virtualenv-init > /dev/null; then
-  eval "`pyenv virtualenv-init -`";
-fi
+# Because pyenv-virtualenv adds to $PROMPT_COMMAND, and I don't like it.
+
+#if which pyenv-virtualenv-init > /dev/null; then
+#  eval "`pyenv virtualenv-init -`";
+#fi
